5c479061dc6d444c52cc55fc0661967ff46312de
Eduardo Melo <eduardo.melo01992@gmail.com>
2024-01-12 12:01:36 -0300

    Second commit

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 # Importando as bibliotecas necessárias
 from flask import Flask, render_template, request
 import pandas as pd
-from plotly.subplots import make_subplots
 import plotly.express as px
 from sklearn.cluster import KMeans
-import json
+import os
 
 # Inicializando a aplicação Flask
 app = Flask(__name__)
@@ -32,46 +31,41 @@ def upload():
         # Aplicando o K-Means
         df_clustered = apply_kmeans(df, n_clusters=8)
 
-        # Gerando subplots e salvando as imagens
-        generate_subplots(df_clustered)
+        # Criando gráficos individuais para cada cluster
+        img_files = create_cluster_plots(df_clustered)
 
-        return render_template('result.html', plot='Clusters processados com sucesso!')
+        return render_template('result.html', img_files=img_files)
 
 # Função para aplicar o K-Means
 def apply_kmeans(df, n_clusters):
     data_for_clustering = df[['horas_ouvidas_rock', 'horas_ouvidas_samba', 'horas_ouvidas_pop', 'horas_ouvidas_rap']]
     kmeans = KMeans(n_clusters=n_clusters, random_state=42)
     df['cluster'] = kmeans.fit_predict(data_for_clustering)
-    return df
+    return df[['horas_ouvidas_rock', 'horas_ouvidas_samba', 'horas_ouvidas_pop', 'horas_ouvidas_rap', 'cluster']]
 
-# Função para gerar subplots e salvar imagens
-def generate_subplots(df):
-    size_factor = 0.5
-    fig = make_subplots(rows=2, cols=2, subplot_titles=('Rock', 'Samba', 'Pop', 'Rap'))
+# Função para criar gráficos individuais para cada cluster e salvar imagens
+def create_cluster_plots(df):
+    img_files = []
+    static_path = os.path.join(app.root_path, 'static')
 
-    for i, estilo in enumerate(['horas_ouvidas_rock', 'horas_ouvidas_samba', 'horas_ouvidas_pop', 'horas_ouvidas_rap']):
-        row = (i // 2) + 1
-        col = (i % 2) + 1
+    for cluster in df['cluster'].unique():
+        cluster_data = df[df['cluster'] == cluster]
 
-        scatter = px.scatter(df, x=estilo, y='cluster', size='horas_ouvidas_pop', opacity=0.7,
-                              labels={estilo: f'Horas de {estilo.capitalize()}', 'cluster': 'Cluster'},
-                              title=f'Perfil de Usuários - {estilo.capitalize()} (Clusters: 8)')
+        # Selecionando o estilo fixo (por exemplo, rock) para o eixo x
+        fixed_style = 'horas_ouvidas_rock'
 
-        scatter.update_traces(marker=dict(size=df['horas_ouvidas_pop'] * size_factor))
+        # Selecionando os outros estilos musicais para o eixo y
+        other_styles = ['horas_ouvidas_samba', 'horas_ouvidas_pop', 'horas_ouvidas_rap']
 
-        # Adicionando a imagem do subplot ao layout
-        fig.add_trace(scatter['data'][0], row=row, col=col)
+        scatter_matrix = px.scatter_matrix(cluster_data, dimensions=[fixed_style] + other_styles,
+                                           color='cluster', title=f'Perfil de Usuários - Cluster {cluster}')
 
-        # Salvando a imagem individualmente
-        img_name = f'dispersao_{estilo}_clusters.png'
-        scatter.write_image(img_name)
+        img_name = f'scatter_matrix_cluster{cluster}.png'
+        img_files.append(img_name)
+        img_path = os.path.join(static_path, img_name)
+        scatter_matrix.write_image(img_path)
 
-    # Atualizando o layout
-    fig.update_layout(height=800, width=1000, title_text="Perfil de Usuários - Estilos Musicais (Clusters: 8)")
-
-    # Salvando a imagem do layout
-    img_name = 'dispersao_clusters.png'
-    fig.write_image(img_name)
+    return img_files
 
 # Executar a aplicação Flask
 if __name__ == '__main__':
